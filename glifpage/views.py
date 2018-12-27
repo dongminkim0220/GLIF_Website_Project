2db02412fec4243c90c771e5b98f877aa97a5291
Your Name <you@example.com>
2018-12-28 01:20:41 +0900

    First commit
    First commit- overall settings

--- glifpage/views.py
+++ glifpage/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'glifpage/index.html', {})
+
+def introduction(request):
+    return render(request, 'glifpage/introduction.html', {})
+
+def announcement(request):
+    return render(request, 'glifpage/announcement.html', {})
+
+def daily(request):
+    return render(request, 'glifpage/daily.html', {})
+
+def inDepthAnalysis(request):
+    return render(request, 'glifpage/inDepthAnalysis.html', {})
+
+def recruiting(request):
+    return render(request, 'glifpage/recruiting.html', {})
