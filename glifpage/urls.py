1fc0af93ac91a2b41fdf17e09b31d92b1682c9cd
Your Name <you@example.com>
2019-01-04 18:33:52 +0900

    take 2
    more css added

--- glifpage/urls.py
+++ glifpage/urls.py
@@ -6,7 +6,12 @@ urlpatterns = [
     url(r'^aboutus/introduction', views.aboutUs, name= 'introduction'),
     url(r'^aboutus/glifers', views.aboutUs, name= 'glifers'),
     url(r'^announcement', views.announcement, name= 'announcement'),
-    url(r'^daily', views.daily, name= 'daily'), 
+    url(r'^daily/overview', views.daily, name= 'overview'),
+    url(r'^daily/news', views.daily, name= 'news'), 
+    url(r'^daily/market', views.daily, name= 'market'), 
     url(r'^indepthanalysis', views.inDepthAnalysis, name= 'inDepthAnalysis'), 
     url(r'^recruiting', views.recruiting, name= 'recruiting'), 
+    url(r'^gliferonly/review', views.gliferOnly, name= 'review'), 
+    url(r'^gliferonly/admin', views.gliferOnly, name= 'admin'), 
+
 ]
--- glifpage/views.py
+++ glifpage/views.py
@@ -14,10 +14,21 @@ def announcement(request):
     return render(request, 'glifpage/announcement.html', {})
 
 def daily(request):
-    return render(request, 'glifpage/daily.html', {})
+    if request.META['PATH_INFO'] == '/daily/overview' :
+        return render(request, 'glifpage/daily/overview.html', {})
+    elif request.META['PATH_INFO'] == '/daily/news' :
+        return render(request, 'glifpage/daily/news.html', {})
+    elif request.META['PATH_INFO'] == '/daily/market' :
+        return render(request, 'glifpage/daily/market.html', {})
 
 def inDepthAnalysis(request):
     return render(request, 'glifpage/inDepthAnalysis.html', {})
 
 def recruiting(request):
     return render(request, 'glifpage/recruiting.html', {})
+
+def gliferOnly(request):
+    if request.META['PATH_INFO'] == '/gliferonly/review' :
+        return render(request, 'glifpage/gliferOnly/review.html', {})
+    elif request.META['PATH_INFO'] == '/gliferonly/admin' :
+        return render(request, 'glifpage/gliferOnly/admin.html', {})
