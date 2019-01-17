c8d8555bcf05476b1b7ba6d04a4a9b93d183b054
Your Name <you@example.com>
2019-01-17 19:37:26 +0900

    indepth RESTful

--- indepthanalysis/models.py
+++ indepthanalysis/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import CustomUser
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 250)
+    attached_file = models.FileField(upload_to = 'indepth_attached_files/%Y/%m/%d/', blank = True, null = True)
+    written_date = models.DateField(auto_now = True)
+    writer = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name = "indepth_writer")
+    content = models.TextField(max_length= 5000)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('indepth-detail', kwargs = {'pk': self.pk})
--- indepthanalysis/urls.py
+++ indepthanalysis/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
 from django.contrib import admin
-from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', PostListView.as_view(), name='indepth-index'),
+    path('<int:pk>', PostDetailView.as_view(), name = "indepth-detail"),
+    path('new', PostCreateView.as_view(), name = "indepth-new"),
+    path('<int:pk>/update', PostUpdateView.as_view(), name = "indepth-update"),
+    path('<int:pk>/delete', PostDeleteView.as_view(), name = "indepth-delete"),
 ]
