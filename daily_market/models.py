ca95527015bf2ba5494342366d44449f5cc3ccd1
Your Name <you@example.com>
2019-01-19 23:38:20 +0900

    market REST

--- daily_market/models.py
+++ daily_market/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from users.models import CustomUser
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    # General Info
+    title = models.CharField(default = "Daily Market Analysis", max_length = 50)
+    attached_file = models.FileField(upload_to = 'Daily_Market_attached_files/%Y/%m/%d/', blank = True, null = True)
+    written_date = models.DateField(auto_now = True)
+    writer = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name = "Daily_Market_writer")
+
+    # Dataset , max_digits = 10
+
+    ## ex , max_digits = 10r
+    e_u_cl = models.DecimalField(decimal_places = 2, blank = True , max_digits = 10)
+    e_u_ch = models.DecimalField(decimal_places = 2, blank = True , max_digits = 10)
+    u_k_cl = models.DecimalField(decimal_places = 2, blank = True , max_digits = 10)
+    u_k_ch = models.DecimalField(decimal_places = 2, blank = True , max_digits = 10)
+
+    ## bon , max_digits = 10d
+    fed_cl = models.DecimalField(decimal_places = 2, blank = True , max_digits = 10)
+    fed_ch = models.DecimalField(decimal_places = 2, blank = True , max_digits = 10)
+    spread_cl = models.DecimalField(decimal_places = 2, blank = True , max_digits = 10)
+    spread_ch = models.DecimalField(decimal_places = 2, blank = True , max_digits = 10)
+    usyr2_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    usyr2_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    usyr10_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    usyr10_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    bok_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    bok_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    kryr3_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    kryr3_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    kryr10_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    kryr10_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+
+    ## stock
+    dj_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    dj_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    sp_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    sp_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    nas_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    nas_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    ksp_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    ksp_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    ksd_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    ksd_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    nik_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    nik_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    hk_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    hk_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+
+    ## Commodity
+    bco_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    bco_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    wti_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    wti_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    gd_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    gd_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+
+    ## etc
+    bdi_cl = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+    bdi_ch = models.DecimalField(decimal_places = 2, blank = True, max_digits = 10)
+
+    def __str__(self) :
+        return self.title
--- daily_market/views.py
+++ daily_market/views.py
@@ -1,5 +1,52 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
-def index(request):
-    return render(request, 'daily_market/index.html', {})
+class PostListView(ListView):
+    model = Post
+    template_name = 'daily_market/index.html'
+    context_object_name = "posts"
+    paginate_by = 5
+    block_size = 5 # 하단의 페이지 목록 수
+
+    
+    def get_queryset(self):
+        order_by_recent_time = Post.objects.all().order_by("-pk")
+        return order_by_recent_time
+
+
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+
+        block_size = 5 # 하단의 페이지 목록 수
+
+        start_index = int((context['page_obj'].number - 1) / self.block_size) * self.block_size
+        end_index = min(start_index + self.block_size, len(context['paginator'].page_range))
+
+        context['page_range'] = context['paginator'].page_range[start_index:end_index]
+
+        return context
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "daily_market/detail.html"
+    context_object_name = "post"
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "daily_market/new.html"
+    fields = "__all__"
+    success_url = reverse_lazy('daily_market-index')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "daily_market/update.html"
+    fields = "__all__"
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "daily_market/delete.html"
+    success_url = reverse_lazy('daily_market-index')
+    
