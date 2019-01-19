1ab1b16fc5398f49cc8810e4b5c56790ffa53a13
Your Name <you@example.com>
2019-01-20 00:10:01 +0900

    daily news REST

--- daily_news/models.py
+++ daily_news/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from users.models import CustomUser
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    # General Info
+    title = models.CharField(default = "Daily News Clipping", max_length = 50)
+    attached_file = models.FileField(upload_to = 'Daily_News_Clipping/%Y/%m/%d/', blank = True, null = True)
+    written_date = models.DateField(auto_now = True)
+    writer = models.ForeignKey(CustomUser, on_delete = models.CASCADE, related_name = "Daily_News_Clipping_writer")
+
+    wsj = "wsj"
+    ft = "ft"
+    bb = "bb"
+    news_CHOICES = (
+        (wsj, "WSJ"),
+        (ft, "FT"),
+        (bb, "BB"),
+    )
+    news_type = models.CharField(
+        max_length = 10 ,
+        choices = news_CHOICES,
+    )
+
+    content_1_kr = models.TextField(max_length = 5000)
+    content_2_kr = models.TextField(max_length = 5000)
+    content_3_kr = models.TextField(max_length = 5000)
+    content_1_en = models.TextField(max_length = 5000)
+    content_2_en = models.TextField(max_length = 5000)
+    content_3_en = models.TextField(max_length = 5000)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('daily_news-detail', kwargs = {'pk': self.pk})
+
--- daily_news/urls.py
+++ daily_news/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
 from django.contrib import admin
-from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', PostListView.as_view(), name='daily_news-index'),
+    path('<int:pk>', PostDetailView.as_view(), name = "daily_news-detail"),
+    path('new', PostCreateView.as_view(), name = "daily_news-new"),
+    path('<int:pk>/update', PostUpdateView.as_view(), name = "daily_news-update"),
+    path('<int:pk>/delete', PostDeleteView.as_view(), name = "daily_news-delete"),
 ]
