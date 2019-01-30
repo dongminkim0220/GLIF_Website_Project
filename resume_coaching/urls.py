15aaa401764f02d1b0780175121660f9e9c350fc
Your Name <you@example.com>
2019-01-30 22:15:30 +0900

    overall refactoring - Resume

--- resume_coaching/urls.py
+++ resume_coaching/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from django.contrib import admin
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='resume_coaching-index'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('new', PostCreateView.as_view(), name = "resume_coaching-new"),
     path('<int:pk>/update', PostUpdateView.as_view(), name = "resume_coaching-update"),
     path('<int:pk>/delete', PostDeleteView.as_view(), name = "resume_coaching-delete"),
+    path('<int:pk>/download', views.download),
 ]
