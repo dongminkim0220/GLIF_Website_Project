f3a5f97547b3aa637c7df9776871892ed9aeb4b7
Your Name <you@example.com>
2019-02-05 23:44:37 +0900

    collectstatic

---
+++ static/resume_coaching/js/resume.js
@@ -0,0 +1,3 @@
+function alertNoPermission() {
+    alert ("멘토 권한이 있어야 다른 사람의 게시물을 확인 할 수 있습니다.")
+}
---
+++ static/setups/js/setups.js
@@ -0,0 +1,3 @@
+function alertNoPermission(){
+    alert("You need to be logged in, and also need to have permission from site-admin")
+}
