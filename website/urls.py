70e4e7f333ac06e1711e976a2f846404c27f5542
Your Name <you@example.com>
2019-02-11 20:32:18 +0900

    media, static settings

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from django.conf import settings
@@ -34,6 +34,9 @@ urlpatterns = [
 
     # ckeditor
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     
 ]
 
