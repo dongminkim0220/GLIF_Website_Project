bda430d91ba4fbbfbae819ce2b68a6d165835bdf
Your Name <you@example.com>
2019-07-02 20:27:19 +0900

    recruiting in admin
    recruiting in admin

--- recruiting/models.py
+++ recruiting/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Recruiting(models.Model):
+    title = models.CharField(max_length = 250)
+    content = models.CharField(max_length = 250)
+    requirements = models.CharField(max_length = 250)
+    deadline_date = models.CharField(max_length = 250)
+    interview_date = models.CharField(max_length = 250)
+    opensession_date = models.CharField(max_length = 250)
--- recruiting/views.py
+++ recruiting/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Recruiting
+from django.views.generic import DetailView
 
 # Create your views here.
 def index(request):
-    return render(request, 'recruiting/index.html', {})
+    recruiting = Recruiting.objects.get(pk = 1)
+    return render(request, 'recruiting/index.html', {'recruiting': recruiting})
